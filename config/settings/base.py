304ee449201d7a55e35108fd126012759191d136
LatifovMuhammadjon <latifov7864@gmail.com>
2026-01-22 11:37:45 +0500

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,11 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Initialize environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 
 # Read .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
